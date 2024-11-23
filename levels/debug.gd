ba4b53d2ffecaebe77ea3c3d9aa144c0c66655c1
Sarah <144068104+iamawatermelo@users.noreply.github.com>
2024-11-23 15:44:07 +0000

    stuff

--- levels/debug.gd
+++ levels/debug.gd
@@ -1,16 +1,36 @@
 extends Node2D
 
-@export var health = 10
-var has_split = false
+@export var prev: Resource = preload("res://levels/debug.tscn")
+@export var next: Resource = preload("res://levels/debug.tscn")
+
+var player = preload("res://player.tscn")
+var can_split = true
+
+signal died
+signal won
+
+
+func on_player_collision(collision: Node2D):
+	print(collision.name)
+	
+	if collision == $DamageTilemap:
+		# kill the player
+		died.emit()
+	elif collision == $WinTilemap:
+		# win
+		won.emit()
 
 func split():
-	has_split = true
+	if not can_split:
+		return
+	
 	var current_player: Node2D = $Player
 	var pos = current_player.position
 	self.remove_child(current_player)
-	var scene = preload("res://player.tscn")
-	var player1 = scene.instantiate()
-	var player2 = scene.instantiate()
+	var player1 = player.instantiate()
+	var player2 = player.instantiate()
+	player1.collided.connect(on_player_collision)
+	player2.collided.connect(on_player_collision)
 	player1.position = pos
 	player1.position.x -= - 8
 	player2.position = pos
@@ -19,13 +39,12 @@ func split():
 	player2.scale = current_player.scale * Vector2(0.5, 0.5)
 	self.add_child(player1)
 	self.add_child(player2)
-
+	
+	can_split = false
 
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("ui_split") and not has_split:
+	if event.is_action_pressed("ui_split"):
 		split()
 
 func _ready() -> void:
-	var intro_level_scene = preload("res://levels/intro.tscn")
-	var intro = intro_level_scene.instantiate()
-	add_child(intro)
+	$Player.collided.connect(on_player_collision)
